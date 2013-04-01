e49fdf4ff8075986f43e85db3c1a83430ad6be7b
Felipe Álvarez <luisfelipealvarez@gmail.com>
2013-04-01 15:05:57 -0300

    Removes Person test to use the one from mysociety/popit-django

--- nuntium/tests/person_test.py
+++
@@ -1,9 +0,0 @@
-from django.test import TestCase
-from nuntium.models import Message
-
-class TestPerson(TestCase):
-
-	def test_create_person(self):
-		person = Person.objects.create(name = 'Name', last_name = 'Last-Name')
-		self.assertTrue(person)
-
