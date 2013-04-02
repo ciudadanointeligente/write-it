4ffa803bbe657867b46be11917415a7a2b55466d
Felipe Álvarez <luisfelipealvarez@gmail.com>
2013-04-02 11:17:34 -0300

    Failing test with home url and template

--- nuntium/tests/__init__.py
+++ nuntium/tests/__init__.py
@@ -1,1 +1,2 @@
 from messages_test import *
+from home_view_test import *
--- nuntium/tests/messages_test.py
+++ nuntium/tests/messages_test.py
@@ -1,9 +1,15 @@
 from django.test import TestCase
 from nuntium.models import Message
+#from popit.models import ApiInstance, Person
 
 class TestMessages(TestCase):
+    # def setUp(self):
+    #     self.instance = ApiInstance(url="http://foo.com/api")
+    #     self.person = Person(api_instance=self.instance, name="Bob")
+    # we should wait for popit-django
 
-	def test_create_message(self):
-		message = Message.objects.create(content = 'message text 1')
-		self.assertTrue(message)
+
+    def test_create_message(self):
+        message = Message.objects.create(content = 'message text 1')
+        self.assertTrue(message)
 
