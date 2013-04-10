ca1f394aaffbf1a5ae1adb58ef5dd2b9853181f4
Pedro Daire <pdaire@gmail.com>
2013-04-10 16:28:37 -0300

    it adds Instance matching popit instance and connect person and messages within that instance

--- nuntium/models.py
+++ nuntium/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils.translation import ugettext as _
 from django.core.exceptions import ValidationError
+from popit.models import ApiInstance
 
 class Message(models.Model):
     """Message: Class that contain the info for a model, despite of the input and the output channels. Subject and content are mandatory fields"""
@@ -12,4 +13,6 @@ class Message(models.Model):
 class Instance(models.Model):
     """Instance: Entity that groups messages and people for usability purposes. E.g. 'Candidates running for president'"""
     name = models.CharField(max_length=255)
+    api_instance = models.ForeignKey(ApiInstance)
+
 		
--- nuntium/tests/instances.py
+++ nuntium/tests/instances.py
@@ -1,19 +1,22 @@
 from django.test import TestCase
 from nuntium.models import Instance, Message
+from popit.models import ApiInstance, Person
 from django.utils.unittest import skip
 
 class InstanceTestCase(TestCase):
 
     def setUp(self):
-        pass
+        self.api_instance1 = ApiInstance.objects.create(url='http://popit.org/api/v1')
+        self.api_instance2 = ApiInstance.objects.create(url='http://popit.org/api/v2')
+        self.person = Person.objects.create(api_instance=self.api_instance1, name= 'Person 1')
 
     def test_create_instance(self):
-        instance = Instance.objects.create(name='instance 1')
+        instance = Instance.objects.create(name='instance 1', api_instance= self.api_instance1)
         self.assertTrue(instance)
 
     def test_instance_containning_several_messages(self):
-        instance1 = Instance.objects.create(name='instance 1')
-        instance2 = Instance.objects.create(name='instance 2')
+        instance1 = Instance.objects.create(name='instance 1', api_instance= self.api_instance1)
+        instance2 = Instance.objects.create(name='instance 2', api_instance= self.api_instance2)
         message1 = Message.objects.create(content='Content 1', subject='Subject 1', instance = instance1)
         message2 = Message.objects.create(content='Content 2', subject='Subject 2', instance = instance1)
         message3 = Message.objects.create(content='Content 3', subject='Subject 3', instance = instance2)
