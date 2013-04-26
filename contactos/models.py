7d4973294093f073db7818760800ff140c3a74b7
Felipe Álvarez <falvarez@votainteligente.cl>
2013-04-26 19:45:41 -0300

    Adding unicodes to most models closes #46

--- contactos/models.py
+++ contactos/models.py
@@ -1,13 +1,23 @@
 from django.db import models
 from popit.models import Person
+from django.utils.translation import ugettext as _
 
 class ContactType(models.Model):
     """This class contain all contact types"""
     name = models.CharField(max_length=255)
     label_name = models.CharField(max_length=255)
 
+    def __unicode__(self):
+    	return self.label_name
+
 class Contact(models.Model):
     """docstring for Contact"""
     contact_type = models.ForeignKey('ContactType')
     person = models.ForeignKey(Person)
     value = models.CharField(max_length=512)
+
+    def __unicode__(self):
+    	return _('%(contact)s (%(type)s)') % {
+	            'contact' : self.value,
+	            'type' : self.contact_type.label_name
+	        }
