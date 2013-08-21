8a822dd9a9c3370ab3f889b2bf01d5d41361e8c1
Felipe Álvarez <luisfelipealvarez@gmail.com>
2013-08-21 18:06:41 -0400

    Autosearching but not sending the form

---
+++ nuntium/templates/nuntium/instance_detail.js
@@ -0,0 +1,39 @@
+console.log("muchas gracias a los cabros de epistemonikos.org por la ayuda con angularjs")
+
+
+var app = angular.module('writeit', []);
+angular.module('writeit').config(function($interpolateProvider) {
+  $interpolateProvider.startSymbol('[[');
+  $interpolateProvider.endSymbol(']]');
+});
+angular.module('writeit').filter('personsHelper', function(){
+	return function(text, length, end) {
+		console.log(text, length, end)
+		return ;
+	}
+})
+app.controller('message-form',function($scope, $http, $filter, $log){
+	$scope.persons = 
+		[{% for person in writeitinstance.persons.all %}
+		{
+			"name": "{{ person }}",
+			"id":{{ person.id }}
+		}
+		{% if not forloop.last %}
+		{# just in case there are still some ie6 around #}
+			,
+		{% endif %}
+		{% endfor %}
+		];
+
+		$scope.comp = function(person){
+			var text = $scope.searchTerm;
+			if($scope.searchTerm == undefined){
+				return true;
+			}
+			var text = $scope.searchTerm
+			text = removeDiacritics((''+text).toLowerCase());
+			var name = removeDiacritics(('' + person.name).toLowerCase())
+          	return name.indexOf(text) > -1
+		};
+	});
