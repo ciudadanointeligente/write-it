513b2f3210f8da5f4ba963610dabf0a78c1b2b3c
Felipe Álvarez <luisfelipealvarez@gmail.com>
2013-08-27 12:26:06 -0400

    Loggin email sending so it can be tracked in the server, besides it was my dream all my life =)

--- mailit/__init__.py
+++ mailit/__init__.py
@@ -6,7 +6,7 @@ from django.conf import settings
 import logging
 from smtplib import SMTPServerDisconnected, SMTPRecipientsRefused, SMTPResponseException
 
-logging.basicConfig(filename='mailing_logger.txt', level=logging.INFO)
+logging.basicConfig(level=logging.INFO)
 
 class MailChannel(OutputPlugin):
     name = 'mail-channel'
