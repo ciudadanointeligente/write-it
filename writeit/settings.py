5c9536da8617268ca8c9918abdf75ecf70b308a7
Pedro Daire <pdaire@gmail.com>
2013-11-18 17:46:07 -0300

    enable bootstrap to the admin site

--- writeit/settings.py
+++ writeit/settings.py
@@ -156,6 +156,7 @@ INSTALLED_APPS = (
     'haystack',
     'djcelery',
     # Uncomment the next line to enable the admin:
+    'django_admin_bootstrapped',
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
