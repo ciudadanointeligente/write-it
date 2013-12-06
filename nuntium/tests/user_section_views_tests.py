0a8201a9618f1b7e382b1028faf4151dcff55f22
Felipe Álvarez <luisfelipealvarez@gmail.com>
2013-12-06 12:18:05 -0300

    Improving testing by testing redirection to login and the next url

--- nuntium/tests/user_section_views_tests.py
+++ nuntium/tests/user_section_views_tests.py
@@ -1,5 +1,5 @@
 from global_test_case import GlobalTestCase as TestCase
-from subdomains.utils import reverse
+from subdomains.utils import reverse, get_domain
 from django.core.urlresolvers import reverse as original_reverse
 from nuntium.models import WriteItInstance
 from django.contrib.auth.models import User
@@ -7,26 +7,50 @@ from django.test.client import Client
 from django.test.client import RequestFactory
 from nuntium.views import WriteItInstanceUpdateView
 from django.forms import ModelForm
+from django.contrib.sites.models import Site
+from django.conf import settings
+from django.utils.translation import activate
 
-class UserViewTestCase(TestCase):
 
+urlconf = settings.SUBDOMAIN_URLCONFS.get(None, settings.ROOT_URLCONF)
+
+class UserSectionTestCase(TestCase):
     def setUp(self):
+        super(UserSectionTestCase, self).setUp()
         self.user = User.objects.create_user(username="fiera", email="fiera@votainteligente.cl", password="feroz")
+
+    # This test should assert that a response redirects to the login interface
+    # and if given a certain next_url then it also tests that
+    # rigth after login it should go to that url
+    def assertRedirectToLogin(self, response, next_url=None):
+
+        self.assertEquals(response.status_code, 302)
+        #when redirecting it always returns the full url based on test server so 
+        #in order to compare it I'll remove the testserver from the url
+        location_this_response_is_taking_us_to = response['Location'].replace("http://testserver","")
+        #the login url comes with the localized url
+        #So I'll set it to english and then remove it from the url
+        activate('en')
+        login_url = original_reverse('django.contrib.auth.views.login', urlconf=urlconf).replace('/en','')
+        self.assertTrue(location_this_response_is_taking_us_to.startswith(login_url))
+        if next_url:
+            current_domain = "http://" + Site.objects.get_current().domain
+            next_url = next_url.replace(current_domain, "")
+            self.assertTrue(location_this_response_is_taking_us_to.endswith("?next="+next_url))
+
+
+class UserViewTestCase(UserSectionTestCase):
 
     def test_account_url_exists(self):
         url = reverse('account')
 
         self.assertTrue(url)
 
-    ## Need to improve this test now I have to GO!
     def test_the_url_is_not_reachable_when_not_logged(self):
         c = Client()
         url = reverse('account')
-        response = c.get(url, follow=True)
-        #[TODO] - This test should not be dependent on the url
-        self.assertRedirects(response, '/en/accounts/login/?next=%2Fen%2Faccounts%2Fprofile')
-        self.assertTemplateUsed(response, "registration/login.html")
-        self.assertTemplateUsed(response, "base.html")
+        response = c.get(url)
+        self.assertRedirectToLogin(response, next_url=url)
 
 
     def test_the_url_exists_and_is_reachable_when_logged(self):
@@ -40,10 +64,10 @@ class UserViewTestCase(TestCase):
         self.assertTemplateUsed(response, "base.html")
 
 
-class WriteitInstanceUpdateTestCase(TestCase):
+class WriteitInstanceUpdateTestCase(UserSectionTestCase):
     def setUp(self):
+        super(WriteitInstanceUpdateTestCase, self).setUp()
         self.factory = RequestFactory()
-        self.user = User.objects.create_user(username="fiera", email="fiera@votainteligente.cl", password="feroz")
         self.writeitinstance = WriteItInstance.objects.all()[0]
 
 
@@ -53,7 +77,8 @@ class WriteitInstanceUpdateTestCase(TestCase):
         self.assertTrue(url)
 
     def test_update_view(self):
-        request = self.factory.get('/customer/details')
+        url = reverse('writeitinstance_update', kwargs={'pk':self.writeitinstance.pk})
+        request = self.factory.get(url)
         request.user = self.user
 
         view = WriteItInstanceUpdateView()
@@ -66,7 +91,7 @@ class WriteitInstanceUpdateTestCase(TestCase):
         url = reverse('writeitinstance_update', kwargs={'pk':self.writeitinstance.pk})
         client = Client()
         response = client.get(url)
-        self.assertEquals(response.status_code, 302) #is redirected
+        self.assertRedirectToLogin(response, next_url=url)
 
 
 
