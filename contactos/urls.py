ea9737fb195bc032b9d42e4b602d40db85920595
Felipe Álvarez <luisfelipealvarez@gmail.com>
2013-12-11 17:37:10 -0300

    Creating a new contact endpoint

--- contactos/urls.py
+++ contactos/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.conf.urls.i18n import i18n_patterns
-from contactos.views import ContactoUpdateView
+from contactos.views import ContactoUpdateView, ContactCreateView
 
 urlpatterns = patterns('',
     url(r'^contacto/update/(?P<pk>[-\d]+)/?$', 
         ContactoUpdateView.as_view(), 
         name = 'contact_value_update'),
+    url(r'^contacto/create/?$', 
+        ContactCreateView.as_view(), 
+        name = 'create-new-contact'),
 )
