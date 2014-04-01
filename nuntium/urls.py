d93bac40e91d5f0f2efeb162bc57f63d88e47452
Felipe Álvarez <falvarez@votainteligente.cl>
2014-04-01 15:14:41 -0300

    decoupling urls

--- nuntium/urls.py
+++ nuntium/urls.py
@@ -4,39 +4,10 @@ from nuntium.views import HomeTemplateView, MessageSearchView,\
     WriteItInstanceDetailView, \
     WriteItInstanceListView
 
-
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
-
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', HomeTemplateView.as_view(template_name='home.html'), name='home'),
     url(r'^instances/?$', WriteItInstanceListView.as_view(template_name='nuntium/template_list.html'), name='instance_list'),
 
     url(r'^search/?$', MessageSearchView(), name='search_messages'),
-    
-    
 )
-from .user_section.views import UserAccountView, WriteItInstanceUpdateView, \
-        YourContactsView, YourInstancesView, WriteItInstanceAdvancedUpdateView, \
-        WriteItInstanceTemplateUpdateView, NewAnswerNotificationTemplateUpdateView, \
-        ConfirmationTemplateUpdateView
-urlpatterns += patterns('',
-    url(r'^accounts/profile/?$', UserAccountView.as_view(), name='account'),
-    url(r'^accounts/your_contacts/?$', YourContactsView.as_view(), name='your-contacts'),
-    url(r'^accounts/your_instances/?$', YourInstancesView.as_view(), name='your-instances'),
-    url(r'^writeitinstance/edit/(?P<pk>[-\d]+)/?$', WriteItInstanceUpdateView.as_view(), name = 'writeitinstance_basic_update'),
-    url(r'^writeitinstance/advanced_edit/(?P<pk>[-\d]+)/?$', \
-        WriteItInstanceAdvancedUpdateView.as_view(), \
-        name = 'writeitinstance_advanced_update'),
-    url(r'^writeitinstance/edit/(?P<pk>[-\d]+)/templates/?$', \
-        WriteItInstanceTemplateUpdateView.as_view(), \
-        name = 'writeitinstance_template_update'),
-    url(r'^writeitinstance/edit/(?P<pk>[-\d]+)/templates/new_answer_notification/?$',
-        NewAnswerNotificationTemplateUpdateView.as_view(),
-        name = 'edit_new_answer_notification_template'),
-    url(r'^writeitinstance/edit/(?P<pk>[-\d]+)/templates/confirmation_template/?$',
-        ConfirmationTemplateUpdateView.as_view(),
-        name = 'edit_confirmation_template'),
-    )
