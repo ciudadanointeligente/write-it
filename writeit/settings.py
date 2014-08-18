dbf35a40ea1e5739c10f5aeeade49f82a7539ffd
Felipe Álvarez <falvarez@votainteligente.cl>
2014-08-18 19:52:05 -0400

    Adding a switch to see if running in heroku or not

--- writeit/settings.py
+++ writeit/settings.py
@@ -299,7 +299,22 @@ try:
 except ImportError:
     pass
 
-try:
-    from heroku_settings import *
-except ImportError:
-    pass
+if 'DATABASE_URL' in os.environ :
+    import dj_database_url
+    DATABASES['default'] =  dj_database_url.config()
+
+    # Honor the 'X-Forwarded-Proto' header for request.is_secure()
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+    # Allow all host headers
+    ALLOWED_HOSTS = ['*']
+
+    # Static asset configuration
+    import os
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    STATIC_ROOT = 'staticfiles'
+    STATIC_URL = '/static/'
+
+    STATICFILES_DIRS = (
+        os.path.join(BASE_DIR, 'static'),
+    )
