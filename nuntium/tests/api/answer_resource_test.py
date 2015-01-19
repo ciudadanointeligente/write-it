a26df889989b8a731bc2f08a646f795c6fbc844f
Duncan Parkes <duncan@mysociety.org>
2015-01-19 20:53:04 +0000

    Remove intermittent test failure.
    I was getting intermittent failures with some of these tests which
    turned out to be due to the random order that postgres gives back
    results in. It's not safe to assume that Answers.objects.all()[0]
    will always be the same thing.

--- nuntium/tests/api/answer_resource_test.py
+++ nuntium/tests/api/answer_resource_test.py
@@ -6,14 +6,13 @@ from django.contrib.auth.models import User
 from popit.models import Person
 from ...api import AnswerResource
 from django.http import HttpRequest
-from ...models import Answer
+from ...models import Answer, Message
 
 
 class AnswersResourceTestCase(ResourceTestCase):
     def setUp(self):
         super(AnswersResourceTestCase, self).setUp()
         call_command('loaddata', 'example_data', verbosity=0)
-        self.answer = Answer.objects.all()[0]
         self.writeitinstance = WriteItInstance.objects.all()[0]
         self.user = User.objects.all()[0]
         self.api_client = TestApiClient()
@@ -26,8 +25,10 @@ class AnswersResourceTestCase(ResourceTestCase):
 
         request = HttpRequest()
         answers_json = self.deserialize(resource.get_list(request))['objects']
-        self.assertEquals(len(answers_json), Answer.objects.count())
-        self.assertEquals(answers_json[0]["id"], self.answer.id)
+        self.assertEquals(
+            set([x.id for x in Answer.objects.all()]),
+            set([x['id'] for x in answers_json])
+            )
 
     def test_get_the_list_of_answers_per_instance(self):
         """Get the list of answers in an writetinstance instance"""
@@ -40,10 +41,11 @@ class AnswersResourceTestCase(ResourceTestCase):
         answers_of_the_writeitinstance = Answer.objects.filter(
             message__writeitinstance=self.writeitinstance
             )
-        self.assertEquals(len(answers), len(answers_of_the_writeitinstance))
-        self.assertEquals(answers[0]['content'], self.answer.content)
-        self.assertEquals(answers[0]['id'], self.answer.id)
-        self.assertEquals(answers[0]['message_id'], self.answer.message.id)
+
+        self.assertEquals(
+            set([(x.id, x.content, x.message_id) for x in answers_of_the_writeitinstance]),
+            set([(x['id'], x['content'], x['message_id']) for x in answers])
+            )
 
     def test_get_only_answers_of_the_instance(self):
         """Show answers from the writeitinstance only"""
@@ -79,26 +81,31 @@ class AnswersResourceTestCase(ResourceTestCase):
         self.assertIn('person', answer_json)
         person = answer_json['person']
 
-        self.assertEquals(person["id"], self.answer.person.id)
-        self.assertEquals(person["image"], self.answer.person.image)
-        self.assertEquals(person["name"], self.answer.person.name)
-        self.assertEquals(person["popit_id"], self.answer.person.popit_id)
-        self.assertEquals(person["popit_url"], self.answer.person.popit_url)
-        self.assertEquals(person["resource_uri"], self.answer.person.popit_url)
-        self.assertEquals(person["summary"], self.answer.person.summary)
+        db_answer = Answer.objects.get(pk=answer_json.get('id'))
+
+        self.assertEquals(person["id"], db_answer.person.id)
+        self.assertEquals(person["image"], db_answer.person.image)
+        self.assertEquals(person["name"], db_answer.person.name)
+        self.assertEquals(person["popit_id"], db_answer.person.popit_id)
+        self.assertEquals(person["popit_url"], db_answer.person.popit_url)
+        self.assertEquals(person["resource_uri"], db_answer.person.popit_url)
+        self.assertEquals(person["summary"], db_answer.person.summary)
 
     def test_answer_ordering(self):
         """The answers are displayed from new to old"""
+        person = Person.objects.all()[0]
+        message = Message.objects.all()[0]
+
         Answer.objects.all().delete()
         answer2 = Answer.objects.create(
-            message=self.answer.message,
+            message=message,
             content="hello this is an answer",
-            person=self.answer.person,
+            person=person,
             )
         answer1 = Answer.objects.create(
-            message=self.answer.message,
+            message=message,
             content="hello this is an answer",
-            person=self.answer.person,
+            person=person,
             )
         answers_json = self.deserialize(AnswerResource().get_list(HttpRequest()))['objects']
         for answer in answers_json:
