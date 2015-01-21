8258d4376acc80f91d91d428164cc09dff9e15fe
Duncan Parkes <duncan@mysociety.org>
2015-01-21 15:53:04 +0000

    Try using env to test on multiple backends in travis

--- writeit/testing_settings.py
+++ writeit/testing_settings.py
@@ -4,35 +4,36 @@ LOCAL_POPIT = True
 LOCAL_ELASTICSEARCH = True
 SOUTH_TESTS_MIGRATE = False
 CELERY_ALWAYS_EAGER = True
-TRAVIS = 'TRAVIS' in os.environ and os.environ['TRAVIS']
+TRAVIS = os.environ.get('TRAVIS')
+
 
 if TRAVIS:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-            'NAME': 'writeit.db',                      # Or path to database file if using sqlite3.
-            # The following settings are not used with sqlite3:
-            'USER': '',
-            'PASSWORD': '',
-            'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-            'PORT': '',                      # Set to empty string for default.
-        },
-        'mysql': {
-            'ENGINE': 'django.db.backends.mysql',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-            'NAME': 'writeit',                      # Or path to database file if using sqlite3.
-            # The following settings are not used with sqlite3:
-            'USER': 'travis',
-            'PASSWORD': '',
-            'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-            'PORT': '',                      # Set to empty string for default.
-        },
-        'postgres': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-            'NAME': 'writeit',                      # Or path to database file if using sqlite3.
+    DB = os.environ.get('DB')
+
+    if DB == 'postgres':
+        default_db = {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'writeit',
             # The following settings are not used with sqlite3:
             'USER': 'postgres',
             'PASSWORD': 'postgres',
-            'HOST': 'localhost',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-            'PORT': '',                      # Set to empty string for default.
-        }
-    }
+            'HOST': 'localhost',
+            'PORT': '',
+            }
+    elif DB == 'mysql':
+        default_db = {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'writeit',
+            # The following settings are not used with sqlite3:
+            'USER': 'travis',
+            'PASSWORD': '',
+            'HOST': '',
+            'PORT': '',
+            }
+    else:
+        default_db = {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': 'writeit.db',                      # Or path to database file if using sqlite3.
+            }
+
+    DATABASES = {'default': default_db}
