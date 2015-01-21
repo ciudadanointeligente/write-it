a774e06cee873d7a9d5f8ff39cc87393bb8a0ac4
Felipe Álvarez <falvarez@votainteligente.cl>
2015-01-21 18:40:20 -0300

    Adds a logging option so we know what's happening in the back

--- nuntium/tasks.py
+++ nuntium/tasks.py
@@ -2,6 +2,10 @@ from celery import task
 from .management.commands.send_mails import send_mails
 from nuntium.models import WriteitInstancePopitInstanceRecord
 from nuntium.popit_api_instance import PopitApiInstance
+import logging
+
+
+logger = logging.getLogger(__name__)
 
 
 @task()
@@ -12,13 +16,14 @@ def send_mails_task():
 @task()
 def pull_from_popit(writeitinstance, popit_api_instance):
     result = writeitinstance._load_persons_from_a_popit_api(popit_api_instance)
+    logger.info(u'Resyncing ' + writeitinstance + u' with ' + popit_api_instance)
     return result
 
 
 @task()
 def update_all_popits():
     all_records = WriteitInstancePopitInstanceRecord.objects.all()
-
+    logger.info(u'Complete resync of all instances')
     for record in all_records:
         popit_api_instance = PopitApiInstance.objects.get(id=record.popitapiinstance.id)
         pull_from_popit(record.writeitinstance, popit_api_instance)
