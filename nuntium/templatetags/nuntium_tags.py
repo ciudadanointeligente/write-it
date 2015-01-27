97a2ef4099df144da12c4a9508692d4d7aa622d5
Felipe Álvarez <falvarez@votainteligente.cl>
2015-01-27 17:03:18 -0300

    Related to #488

---
+++ nuntium/templatetags/nuntium_tags.py
@@ -0,0 +1,11 @@
+from django import template
+from contactos.models import Contact
+from django.db.models import Q
+
+register = template.Library()
+
+
+def show_contacts_for(person, writeitinstance):
+    contacts = Contact.objects.filter(Q(person=person), Q(writeitinstance=writeitinstance))
+    return {"contacts": contacts}
+register.inclusion_tag('nuntium/profiles/contacts/show_contacts_in.html')(show_contacts_for)
