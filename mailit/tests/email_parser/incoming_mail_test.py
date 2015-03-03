88b74be7a13a17a987566f6f9a1b2dcb602a510f
Felipe Álvarez <falvarez@votainteligente.cl>
2015-03-03 18:35:27 -0300

    Moving AnswerHandlerTestCase to its own test file

--- mailit/tests/email_parser/incoming_mail_test.py
+++ mailit/tests/email_parser/incoming_mail_test.py
@@ -43,46 +43,6 @@ class TestApiKeyAuthentification(TestCase):
         self.assertEquals(req.headers['Authorization'], expected_headers)
 
 
-class AnswerHandlerTestCase(TestCase):
-    def setUp(self):
-        super(AnswerHandlerTestCase, self).setUp()
-        self.user = User.objects.all()[0]
-        self.where_to_post_creation_of_the_answer = 'http://writeit.ciudadanointeligente.org/api/v1/create_answer/'
-        config.WRITEIT_API_ANSWER_CREATION = self.where_to_post_creation_of_the_answer
-        config.WRITEIT_API_KEY = self.user.api_key.key
-        config.WRITEIT_USERNAME = self.user.username
-
-    def test_class_answer(self):
-
-        email_answer = EmailAnswer()
-        self.assertIsNone(email_answer.message_id)
-        email_answer.subject = 'prueba4'
-        email_answer.content_text = 'prueba4lafieritaespeluda'
-        email_answer.outbound_message_identifier = '8974aabsdsfierapulgosa'
-        email_answer.email_from = 'falvarez@votainteligente.cl'
-        email_answer.when = 'Wed Jun 26 21:05:33 2013'
-        email_answer.message_id = '<CAA5PczfGfdhf29wgK=8t6j7hm8HYsBy8Qg87iTU2pF42Ez3VcQ@mail.gmail.com>'
-
-        self.assertTrue(email_answer)
-        self.assertEquals(email_answer.subject, 'prueba4')
-        self.assertEquals(email_answer.content_text, 'prueba4lafieritaespeluda')
-        self.assertEquals(email_answer.outbound_message_identifier, '8974aabsdsfierapulgosa')
-        self.assertEquals(email_answer.email_from, 'falvarez@votainteligente.cl')
-        self.assertEquals(email_answer.when, 'Wed Jun 26 21:05:33 2013')
-        self.assertEquals(email_answer.message_id, '<CAA5PczfGfdhf29wgK=8t6j7hm8HYsBy8Qg87iTU2pF42Ez3VcQ@mail.gmail.com>')
-        self.assertFalse(email_answer.is_bounced)
-
-    @skip("not yet I'm going to do something")
-    def test_getter_removes_the_identifier(self):
-        email_answer = EmailAnswer()
-        email_answer.subject = 'prueba4'
-        email_answer.outbound_message_identifier = '8974aabsdsfierapulgosa'
-        email_answer.content_text = 'prueba4lafieritaespeluda y lo mandé desde este mail devteam+8974aabsdsfierapulgosa@chile.com'
-
-        self.assertFalse(email_answer.outbound_message_identifier in email_answer.content_text)
-        self.assertNotIn("devteam+@chile.com", email_answer.content_text)
-
-
 class ReplyHandlerTestCase(ResourceTestCase):
     def setUp(self):
         super(ReplyHandlerTestCase, self).setUp()
