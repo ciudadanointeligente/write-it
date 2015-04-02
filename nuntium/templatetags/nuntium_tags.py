759f8b4f9b74f56a5b4ff92af5f1345e7184ea12
Chris Mytton <chrism@mysociety.org>
2015-04-02 13:55:27 +0100

    Add template tag to join a list with commas
    This is frequently useful when showing a list of people that a message
    was written to.

--- nuntium/templatetags/nuntium_tags.py
+++ nuntium/templatetags/nuntium_tags.py
@@ -13,3 +13,20 @@ def show_contacts_for(person, writeitinstance):
         "writeitinstance": writeitinstance
     }
 register.inclusion_tag('nuntium/profiles/contacts/show_contacts_in.html')(show_contacts_for)
+
+@register.filter
+def join_with_commas(obj_list):
+    """Takes a list of objects and returns their unicode representations,
+    seperated by commas and with 'and' between the penultimate and final items
+    For example, for a list of fruit objects:
+    [<Fruit: apples>,<Fruit: oranges>,<Fruit: pears>] -> 'apples, oranges and pears'
+    """
+
+    l = len(obj_list)
+    if l == 0:
+        return ""
+    elif l == 1:
+        return u"%s" % obj_list[0]
+    else:
+        return ", ".join(unicode(obj) for obj in obj_list[:l - 1]) \
+                + " and " + unicode(obj_list[l - 1])
