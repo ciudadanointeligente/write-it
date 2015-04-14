8ab37a4daf484867deff6353fab90dbfa13516bb
Dave Whiteland <dave@mysociety.org>
2015-04-14 14:48:21 +0100

    add more URLs to the welcome page/view

--- nuntium/user_section/views.py
+++ nuntium/user_section/views.py
@@ -602,4 +602,6 @@ class WelcomeView(DetailView):
         # passing URLs in for easy insertion into the translation tags
         context['url_template'] = reverse('writeitinstance_template_update', subdomain=self.request.subdomain)
         context['url_basic_update'] = reverse('writeitinstance_basic_update', subdomain=self.request.subdomain)
+        context['url_maxrecipients_update'] = reverse('writeitinstance_maxrecipients_update', subdomain=self.request.subdomain)
+        context['url_data_sources'] = reverse('relate-writeit-popit', subdomain=self.request.subdomain)
         return context
