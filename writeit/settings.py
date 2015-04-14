097c3ead020c3818337664f49a4e4d01507058ea
Duncan Parkes <duncan@mysociety.org>
2015-04-14 15:15:53 +0100

    CommonMiddleware must come after LocaleMiddleware
    See https://docs.djangoproject.com/en/1.6/ref/middleware/#middleware-ordering.
    
    This could cause get_language to produce the wrong language, and perhaps
    be the reason why we sometimes get en-us in urls where we don't want it.

--- writeit/settings.py
+++ writeit/settings.py
@@ -155,12 +155,12 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     )
 
 MIDDLEWARE_CLASSES = (
-    'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.locale.LocaleMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'pagination.middleware.PaginationMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
