42e2657349698df2c1c122aeda42541cdf338fdb
Felipe Álvarez <falvarez@votainteligente.cl>
2015-04-30 18:40:51 -0300

    Adding an extra templatetag that works as a wrapper on top of subdomains.templatetags.subdomainurls.url

--- nuntium/user_section/tests/template_tags_tests.py
+++ nuntium/user_section/tests/template_tags_tests.py
@@ -2,6 +2,8 @@ from django.template import Context, Template
 from global_test_case import GlobalTestCase as TestCase
 from contactos.models import Contact
 from popit.models import Person
+from subdomains.utils import reverse
+from django.template import TemplateSyntaxError
 
 
 class ListContactsTemplateTag(TestCase):
@@ -40,3 +42,26 @@ class ListContactsTemplateTag(TestCase):
             })
         rendered = t.render(c)
         self.assertEquals(u'Pedro, Marcel and Felipe', rendered)
+
+    def test_get_url_using_subdomain(self):
+        '''
+        There is a problem with the subdomain urls wich doesn't allow me to do the following
+        '''
+
+        with self.assertRaises(TemplateSyntaxError):
+            t = Template("{% load subdomainurls %}{% url 'contact_us' subdomain=None as contact_page %}{{contact_page}}")
+            t.render(Context({}))
+
+        '''
+        which is particularly useful when I want to include that url in the blocktrans
+        tag as described in
+
+        https://docs.djangoproject.com/en/1.6/topics/i18n/translation/#blocktrans-template-tag
+        So I was thinking of creating another templatetag to do so.
+        '''
+        expected_template_rendered = reverse('contact_us', subdomain=None)
+
+        t = Template("{% load nuntium_tags %}{% assignment_url_with_subdomain 'contact_us' subdomain=None as contact_page %}{{contact_page}}")
+        actual_template_rendered = t.render(Context({}))
+
+        self.assertEquals(expected_template_rendered, actual_template_rendered)
