6b6d0ebb69f35b124c71d6b05964e386ea8f4155
Matthew Somerville <matthew-github@dracos.co.uk>
2015-12-24 14:07:01 +0000

    Update source URL field.
    Fixes #1106.

--- nuntium/user_section/forms.py
+++ nuntium/user_section/forms.py
@@ -246,7 +246,7 @@ class AnswerForm(ModelForm):
 
 class RelatePopitInstanceWithWriteItInstance(Form, PopitParsingFormMixin):
     popit_url = URLField(
-        label=_('PopIt URL'),
+        label=_('Popolo URL'),
         help_text=_("Example: https://cdn.rawgit.com/everypolitician/everypolitician-data/1460373/data/Abkhazia/Assembly/ep-popolo-v1.0.json"),
         )
 
@@ -264,7 +264,7 @@ class RelatePopitInstanceWithWriteItInstance(Form, PopitParsingFormMixin):
         cleaned_data = super(RelatePopitInstanceWithWriteItInstance, self).clean(*args, **kwargs)
         if self.writeitinstance.writeitinstancepopitinstancerecord_set.filter(popitapiinstance__url=cleaned_data.get('popit_url')):
             self.relate()
-            raise ValidationError(_("You have already added this PopIt. But we will fetch the data from it again now."))
+            raise ValidationError(_("You have already added this source. But we will fetch the data from it again now."))
         return cleaned_data
 
 
