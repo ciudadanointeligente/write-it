b6a960a107582158a118e1f5dad4fef023b12e2b
Chris Mytton <chrismytton@gmail.com>
2016-03-30 13:29:29 +0200

    Prevent the language from getting stuck in the session
    We're using a slight hack to work around the fact that Django doesn't
    allow you to hook into the language selection process in middleware. We
    check if the user has specified a default language in their session, if
    they haven't then we temporarily use that as a place to store the
    default lanuage for a session. We then delete our override from the
    session so it doesn't persist when the user is viewing other instances.

--- nuntium/middleware.py
+++ nuntium/middleware.py
@@ -13,9 +13,21 @@ class InstanceLocaleMiddleware(LocaleMiddleware):
         except WriteItInstanceConfig.DoesNotExist:
             default_language = settings.LANGUAGE_CODE
 
+        # Django 1.6 doesn't provide a nice way to hook into the language
+        # selection process to override it with a default. So instead we add
+        # a default language to the session and then remove it again after
+        # running 'get_language_from_request'.
+        did_override_session_language = False
+
         if not request.session.get('django_language', None):
             request.session['django_language'] = default_language
+            did_override_session_language = True
+
         language = translation.get_language_from_request(
             request, check_path=check_path)
+
+        if did_override_session_language:
+            del request.session['django_language']
+
         translation.activate(language)
         request.LANGUAGE_CODE = translation.get_language()
