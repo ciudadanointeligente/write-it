1fd0297c93f3e21f9d7d26cf432040c732ffd503
Chris Mytton <chrismytton@gmail.com>
2016-03-30 16:07:44 +0100

    Don't use session hack in locale middleware
    Rather than using the session as a temporary holding space for our
    language preference we instead override get_language_from_request and
    add in some custom behaviour for looking in the database.
    
    This also uses OrderedDict from python as SortedDict in django has been
    deprecated.

--- nuntium/middleware.py
+++ nuntium/middleware.py
@@ -1,3 +1,5 @@
+from collections import OrderedDict
+
 from django.middleware.locale import LocaleMiddleware
 from django.utils import translation
 from django.conf import settings
@@ -5,29 +7,30 @@ from django.conf import settings
 from nuntium.models import WriteItInstanceConfig
 
 
+def get_language_from_request(request, check_path=False):
+    supported = OrderedDict(settings.LANGUAGES)
+
+    if check_path:
+        lang_code = translation.get_language_from_path(request.path_info, supported)
+        if lang_code is not None:
+            return lang_code
+
+    try:
+        lang_code = WriteItInstanceConfig.objects.get(writeitinstance__slug=request.subdomain).default_language
+    except WriteItInstanceConfig.DoesNotExist:
+        lang_code = None
+
+    if lang_code in supported and lang_code is not None and translation.check_for_language(lang_code):
+        return lang_code
+
+    # Call with check_path False as we've already done that above!
+    return translation.get_language_from_request(request, check_path=False)
+
+
 class InstanceLocaleMiddleware(LocaleMiddleware):
     def process_request(self, request):
+        """Same as parent, except calling our own get_language_from_request"""
         check_path = self.is_language_prefix_patterns_used()
-        try:
-            default_language = WriteItInstanceConfig.objects.get(writeitinstance__slug=request.subdomain).default_language
-        except WriteItInstanceConfig.DoesNotExist:
-            default_language = settings.LANGUAGE_CODE
-
-        # Django 1.6 doesn't provide a nice way to hook into the language
-        # selection process to override it with a default. So instead we add
-        # a default language to the session and then remove it again after
-        # running 'get_language_from_request'.
-        did_override_session_language = False
-
-        if not request.session.get('django_language', None):
-            request.session['django_language'] = default_language
-            did_override_session_language = True
-
-        language = translation.get_language_from_request(
-            request, check_path=check_path)
-
-        if did_override_session_language:
-            del request.session['django_language']
-
+        language = get_language_from_request(request, check_path=check_path)
         translation.activate(language)
         request.LANGUAGE_CODE = translation.get_language()
