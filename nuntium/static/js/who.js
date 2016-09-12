ecd28fd1384deb7946e7603c8480a8e0bb2b1a80
Zarino Zappia <mail@zarino.co.uk>
2016-09-12 12:00:46 +0100

    Styling for "uncontactable" chosen.js dropdown options

--- nuntium/static/js/who.js
+++ nuntium/static/js/who.js
@@ -1,5 +1,26 @@
 $(function(){
 
+  // Temporarily add the "uncontactable" class to a random selection
+  // of contacts in the person selector.
+  // TODO: This should be done in forms.py > PersonMultipleChoiceField
+  $('.chosen-person-select option').each(function(i){
+    if( Math.random(0, 1) <= 0.2 ){
+      $(this).addClass('uncontactable');
+    }
+  });
+
+  // Copy the "uncontactable" class from the original dropdown `<option>`s,
+  // onto the chosen.js dropdown `<li>`s.
+  $('.chosen-person-select').on('chosen:showing_dropdown', function(e, params){
+    var $options = $(params.chosen.dropdown[0]).find('li');
+    $options.each(function(i){
+      var originalOptElement = params.chosen.results_data[i];
+      if(originalOptElement.classes.indexOf('uncontactable') !== -1){
+        $(this).addClass('uncontactable');
+      }
+    });
+  });
+
   if(window.who.rtl) {
     $(".chosen-person-select").addClass('chosen-rtl');
   }
@@ -10,6 +31,6 @@ $(function(){
     placeholder_text_multiple: ' ', // hide placeholder text
     placeholder_text_single: ' ', // hide placeholder text
     single_backstroke_delete: false
-   });
+  });
 
 });
