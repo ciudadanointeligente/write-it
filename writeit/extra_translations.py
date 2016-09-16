c7e513e80a11fddca517a7bccf35b6ef298b5f5e
Mark Longair <mhl@pobox.com>
2016-09-16 12:37:31 +0100

    Add a new string for translation to override it in core Django

---
+++ writeit/extra_translations.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+# This module exists just to list strings for translation to be picked
+# up by makemessages.
+
+from __future__ import unicode_literals
+
+from django.utils.translation import ugettext as _
+
+# This is so that we can override the default translations in core
+# Django:
+
+_('This field is required.')
