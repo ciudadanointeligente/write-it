b75c539ed91a35561345b674afec408849601933
Mark Longair <mhl@pobox.com>
2017-04-04 14:57:35 +0100

    mySociety deployment: changes for timestamped deploy
    Our deployment system can now support lower downtime deploys where
    everything except migrating the database happens while the site is still
    up, being prepared in a new deployment directory whose name is based on
    the current timestamp. To take advantage of this, we need to switch the
    virtualenv and STATIC_ROOT to be a directories within the repo, and
    split the deployment script into two.

---
+++ conf/pre_deploy_actions.bash
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# abort on any errors
+set -e
+
+# check that we are in the expected directory
+cd `dirname $0`/..
+
+# create/update the virtual environment
+
+virtualenv_dir=.venv
+virtualenv_activate="$virtualenv_dir/bin/activate"
+
+if [ ! -f "$virtualenv_activate" ]
+then
+    # NOTE: some packages are difficult to install if they are not site packages,
+    # for example xapian. If using these you might want to add the
+    # '--enable-site-packages' argument.
+    virtualenv --no-site-packages "$virtualenv_dir"
+fi
+
+source "$virtualenv_activate"
+
+# Upgrade pip to a secure version
+# curl -L -s https://raw.github.com/pypa/pip/master/contrib/get-pip.py | python
+# Revert to the line above once we can get a newer setuptools from Debian, or
+# pip ceases to need such a recent one.
+curl -L -s https://raw.github.com/mysociety/commonlib/master/bin/get_pip.bash | bash
+
+pip install --requirement requirements.txt
+pip install --requirement requirements-mysociety.txt
+
+# make sure that there is no old code (the .py files may have been git deleted)
+find . -name '*.pyc' -delete
+
+cp conf/local_settings.py writeit/local_settings.py
+
+./manage.py collectstatic --noinput
